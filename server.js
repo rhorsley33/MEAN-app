0bcbc6d95b224c3a67e111c4df498d7c38cce02c
Robby Horsley <robbyhorsley@Robbys-MacBook-Pro.local>
2019-06-18 18:24:32 -0600

    MERN app preping to deploy

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
 const app = express();
+const path = require('path');
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -17,6 +20,16 @@ mongoose.connect(db)
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
